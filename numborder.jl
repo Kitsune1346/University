07105b99c3d5485e14caf4726d4bff9f63acb7f6
Kitsune1346 <99029564+Kitsune1346@users.noreply.github.com>
2023-12-22 13:21:03 +0300

    Add files via upload

---
+++ numborder.jl
@@ -0,0 +1,59 @@
+using  HorizonSideRobots
+r=Robot(animate=true)
+
+function num_border(r::Robot)
+    num=0
+    num_ost=num_side(r, Ost)
+    num_sud=num_side(r, Sud)
+    side=West
+    while isborder(r, Nord)==false
+        num+=along_with_num_borders(r, side)
+        move!(r, Nord)
+        side=inverse(side)
+    end
+    along!(r, side)
+    along!(r, Ost)
+    along!(r, Sud)
+    along(r, Nord, num_sud)
+    along(r, West, num_ost)
+    return num
+end
+function along_with_num_borders(r::Robot, side::HorizonSide)
+    n=0
+    flag=false
+    while isborder(r, side)==false
+        move!(r, side)
+        if isborder(r, Nord)==true
+            flag=true
+        end
+        if flag==true && isborder(r, Nord)==false
+            n+=1
+            flag=false
+        end
+        
+    end
+    return n
+end
+function num_side(r::Robot, side::HorizonSide)
+    n=0
+    while isborder(r, side)==false
+        move!(r, side)
+        n+=1
+    end
+    return n
+    
+end
+function along!(r::Robot, side::HorizonSide)
+    while isborder(r, side)==false
+        move!(r, side)
+    end
+end
+
+function along(r::Robot, side, num)
+    while num!=0
+        move!(r::Robot, side)
+        num-=1
+    end
+end
+
+inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,  4))
